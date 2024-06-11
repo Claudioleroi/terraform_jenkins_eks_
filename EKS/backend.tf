0bde4cb81f3eb5cfdc37a296e89b26d0321cf85c
Claudioleroi <ikambacheryl7@gmail.com>
2024-06-11 16:51:12 +0100

    1st commit

---
+++ EKS/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "claudiostate3"
+    region = "us-east-1"
+    key = "claude_key/terraform.tfstate"
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# terraform_jenkins_eks_
+# terraform_jenkins_eks
+conception d'une pipeline avec  jenkins 
